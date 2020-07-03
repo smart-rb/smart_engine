e860f7b23f2059239cadbd2c5633032ecdf2f771
Rustam Ibragimov <iamdaiver@gmail.com>
2020-07-03 12:38:34 +0300

    [code] documentation updates + version bump

--- lib/smart_core/engine/atom.rb
+++ lib/smart_core/engine/atom.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 # @api public
-# @since 0.1.0
+# @since 0.7.0
 class SmartCore::Engine::Atom
-  # @param initial_value [NilClass]
+  # @param initial_value [Any]
   # @return [void]
   #
   # @api private
-  # @since 0.1.0
+  # @since 0.7.0
   def initialize(initial_value = nil)
     @value = initial_value
     @barrier = SmartCore::Engine::Lock.new
@@ -16,21 +16,27 @@ class SmartCore::Engine::Atom
   # @return [Any]
   #
   # @api public
-  # @since 0.1.0
+  # @since 0.7.0
   def value
     with_barrier { @value }
   end
 
+  # @param block [Block]
   # @return [Any]
   #
   # @api public
-  # @since 0.1.0
+  # @since 0.7.0
   def swap(&block)
     with_barrier { @value = yield(@value) }
   end
 
   private
 
+  # @param block [Block]
+  # @return [Any]
+  #
+  # @api private
+  # @since 0.1.0
   def with_barrier(&block)
     @barrier.synchronize(&block)
   end
